43c2e84911c693cf778607a01b1dbc2097a95c1f
makotos3107 <sdf821m@gmail.com>
2021-05-25 18:59:23 +0900

    fizzbuzz

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,16 @@
+def FizzBuzz(num)
+    (1..num).each do |number|
+        if number % 15 == 0
+            puts 'FizzBuzz'
+        elsif number % 5 == 0
+            puts 'Buzz'
+        elsif number % 3 == 0
+            puts 'Fizz'
+        else
+            puts number
+        end
+    end
+end
+
+num_max = 100
+FizzBuzz(num_max)
